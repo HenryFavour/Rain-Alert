7f8eabc3e062b44f519c3a866406801804c37669
HenryFavour <benjaminfavour2000@gmail.com>
2026-03-13 09:43:13 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ password = os.environ.get("EMAIL_PASSWORD")
 r_email =  os.environ.get("RECEIVERS_EMAIL")
 
 api_endpoint = "http://api.openweathermap.org/data/2.5/forecast"
-api_key = "b6427eaaf03fb6f4fae376ab81b6e903"
+api_key = os.environ.get("OW_API_KEY")
 parameters = {
     "lat": 9.081999 ,
     "lon": 8.675277,
@@ -38,6 +38,7 @@ if will_rain:
     connection.starttls()
     connection.login(user= my_email, password= password)
     connection.sendmail(from_addr= my_email, to_addrs= r_email, msg="Subject:Rain Alert\n\nThere will be rain today, take an umbrella.")
+
 
 
 
